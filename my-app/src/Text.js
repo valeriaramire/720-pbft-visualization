d5cd0276c95006b9e4291e3fda113eeca9a9ced1
Apurvata Kumar <81375992+apurvata@users.noreply.github.com>
2025-11-23 15:14:07 -0500

    Added switch and slider

--- my-app/src/Text.js
+++ my-app/src/Text.js
@@ -1,13 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import 'devextreme/dist/css/dx.light.css';
 import { TextBox } from 'devextreme-react/text-box';
 import { Switch } from "devextreme-react/switch";
 import RangeSlider, { Tooltip, Label } from 'devextreme-react/range-slider';
 
-class Text extends React.Component {
-    render() {
+function Text() {
+        const onValueChanged = useCallback((e) => {
+            // https://js.devexpress.com/React/Documentation/Guide/UI_Components/Switch/Getting_Started_with_Switch/
+            const stateLabel = e.value ? "ON" : "OFF";
+            
+        }, []);
         return (
             <div>
                 <div>
@@ -15,11 +19,24 @@ class Text extends React.Component {
                     <Switch
                         width={80}
                         rtlEnabled={true}
+                        onValueChanged={onValueChanged}
                     />
                 </div>
+                <h4>
+                    Set the number of Replicas
+                </h4>
+                <RangeSlider min={0} max={50} showRange={true} width={500}>
+                    <Tooltip
+                        enabled={true}
+                        showMode="always"
+                        position="bottom"
+                    />
+                </RangeSlider>
+                
             </div>
         );
     }
-}
 
 export default Text;
+
+//https://js.devexpress.com/React/Demos/WidgetsGallery/Demo/RangeSlider/Overview/MaterialBlueLight/
